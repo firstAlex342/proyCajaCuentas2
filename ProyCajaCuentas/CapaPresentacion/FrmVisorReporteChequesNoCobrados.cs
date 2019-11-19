74fe7562cbe1d1280613b43b7d691165319ad0b8
heaven78 <fixer90210@gmail.com>
2019-11-19 11:24:40 -0500

    FrmVisorReporteChequesNoCobrados: Se incorporra progressbar , async, await

--- ProyCajaCuentas/CapaPresentacion/FrmVisorReporteChequesNoCobrados.cs
+++ ProyCajaCuentas/CapaPresentacion/FrmVisorReporteChequesNoCobrados.cs
@@ -28,14 +28,102 @@ namespace CapaPresentacion
             return (fecha.ToShortDateString());
         }
 
+        private async Task<int> CargarCRViewerAsync(DateTime fechaInicio, DateTime fechaFin)
+        {
+            //https://www.codeproject.com/Articles/10173/Loading-Crystal-Report-reports-which-use-Stored-Pr
+            CRReporteChequesNoCobrados crReporte = new CRReporteChequesNoCobrados();
+            crReporte.SetDatabaseLogon("sa", "modomixto", "CRUZ2-THINK", "DBCajaCuentas2");
+            crReporte.SetParameterValue("@fechaInicio", fechaInicio);
+            crReporte.SetParameterValue("@fechaFin", fechaFin);
+
+            TextObject periodoDeBusquedaTextObject = crReporte.ReportDefinition.ReportObjects["Text12"] as TextObject;
+            periodoDeBusquedaTextObject.Text = "periodo " + MuestraFechaDeBusquedaSinLaHora(fechaInicio) + " a " + MuestraFechaDeBusquedaSinLaHora(fechaFin);
+            crystalReportViewer1.ReportSource = crReporte;
+
+            await Task.Delay(200);
+            return (1);
+        }
+
+        private void DeshabilitarButtonsYDateTimePicker()
+        {
+            dateTimePicker2.Enabled = false;
+            dateTimePicker3.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        private void HabilitarButtonsYDateTimePicker()
+        {
+            dateTimePicker2.Enabled = true;
+            dateTimePicker3.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void IniciarProgressBar()
+        {
+            progressBar1.Visible = true;
+            progressBar1.Value = 0;
+            progressBar1.MarqueeAnimationSpeed = 30;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+        }
+
+        private void DetenerProgressBar()
+        {
+            progressBar1.Value = 0;
+            progressBar1.MarqueeAnimationSpeed = 100;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            progressBar1.Visible = false;
+        }
+
+        private async Task<int> GenerarReporteExcelAsync(DateTime fechaInicio, DateTime fechaFin, string nomArchivo)
+        {
+            //http://aspalliance.com/478_Exporting_to_Excel_in_Crystal_Reports_NET__Perfect_Excel_Exports.3
+            //https://www.c-sharpcorner.com/UploadFile/mahesh/savefiledialog-in-C-Sharp/
+            CRReporteChequesNoCobradosParaExportar reporte = new CRReporteChequesNoCobradosParaExportar();
+            reporte.SetDatabaseLogon("sa", "modomixto", "CRUZ2-THINK", "DBCajaCuentas2");
+            reporte.SetParameterValue("@fechaInicio", fechaInicio);
+            reporte.SetParameterValue("@fechaFin", fechaFin);
+
+            //Ponerle las fechas de busqueda al reporte
+            TextObject periodoDeBusquedaTextObject = reporte.ReportDefinition.ReportObjects["Text19"] as TextObject;
+            periodoDeBusquedaTextObject.Text = MuestraFechaDeBusquedaSinLaHora(fechaInicio) + " a " + MuestraFechaDeBusquedaSinLaHora(fechaFin);
+
+
+            // Declare variables and get the export options.
+            ExportOptions exportOpts = new ExportOptions();
+            ExcelFormatOptions excelFormatOpts = new ExcelFormatOptions();
+            DiskFileDestinationOptions diskOpts = new DiskFileDestinationOptions();
+            exportOpts = reporte.ExportOptions;
+            // Set the excel format options.
+            excelFormatOpts.ExcelUseConstantColumnWidth = false;
+            excelFormatOpts.ShowGridLines = true;
+
+            //exportOpts.ExportFormatType = ExportFormatType.ExcelRecord;
+            exportOpts.ExportFormatType = ExportFormatType.Excel;
+            exportOpts.FormatOptions = excelFormatOpts;
+            // Set the disk file options and export.
+            exportOpts.ExportDestinationType = ExportDestinationType.DiskFile;
+            //diskOpts.DiskFileName = "miotroreporttttte.xls";
+            diskOpts.DiskFileName = nomArchivo;
+            exportOpts.DestinationOptions = diskOpts;
+            reporte.Export();
+
+            await Task.Delay(200);
+            return (1);
+        }
+
+
         //------------------Methods controller
-        private DataTable Cheque_RecuperarDetallesDeChequesNoCobradosController(DateTime fechaInicio, DateTime fechaFin)
+        private async Task<DataTable> Cheque_RecuperarDetallesDeChequesNoCobradosControllerAsync(DateTime fechaInicio, DateTime fechaFin)
         {
             ClsCheque clsCheque = new ClsCheque();
             clsCheque.FechaAlta = fechaInicio;
             clsCheque.FechaModificacion = fechaFin;
 
-            return (clsCheque.Cheque_RecuperarDetallesDeChequesNoCobrados());
+            DataTable res = clsCheque.Cheque_RecuperarDetallesDeChequesNoCobrados();
+            await Task.Delay(200);
+            return (res);
         }
 
         //---------------------------Events
@@ -44,13 +132,15 @@ namespace CapaPresentacion
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             try
             {
+                DeshabilitarButtonsYDateTimePicker();
+                IniciarProgressBar();
+
                 DateTime fechaInicio;
                 DateTime fechaFin;
-
 
                 fechaInicio = new DateTime(dateTimePicker2.Value.Year, dateTimePicker2.Value.Month,
                         dateTimePicker2.Value.Day, 0, 1, 0);
@@ -58,24 +148,21 @@ namespace CapaPresentacion
                 fechaFin = new DateTime(dateTimePicker3.Value.Year, dateTimePicker3.Value.Month,
                         dateTimePicker3.Value.Day, 23, 59, 58);
 
-                DataTable tablaNatural = Cheque_RecuperarDetallesDeChequesNoCobradosController(fechaInicio, fechaFin);
-                if(tablaNatural.Rows.Count == 0)
+                DataTable tablaNatural = await Cheque_RecuperarDetallesDeChequesNoCobradosControllerAsync(fechaInicio, fechaFin);
+
+                if (tablaNatural.Rows.Count == 0)
                 {
                     crystalReportViewer1.ReportSource = null;
+                    DetenerProgressBar();
+                    HabilitarButtonsYDateTimePicker();
                     MessageBox.Show("Se encontraron 0 cheques sin fecha de cobro, en el rango de fechas solicitado", "Resultado de operación", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
 
                 else
                 {
-                    //https://www.codeproject.com/Articles/10173/Loading-Crystal-Report-reports-which-use-Stored-Pr
-                    CRReporteChequesNoCobrados crReporte = new CRReporteChequesNoCobrados();
-                    crReporte.SetDatabaseLogon("sa", "modomixto", "CRUZ2-THINK", "DBCajaCuentas2");
-                    crReporte.SetParameterValue("@fechaInicio", fechaInicio);
-                    crReporte.SetParameterValue("@fechaFin", fechaFin);
-
-                    TextObject periodoDeBusquedaTextObject = crReporte.ReportDefinition.ReportObjects["Text12"] as TextObject;
-                    periodoDeBusquedaTextObject.Text = "periodo " + MuestraFechaDeBusquedaSinLaHora(fechaInicio) + " a " + MuestraFechaDeBusquedaSinLaHora(fechaFin);
-                    crystalReportViewer1.ReportSource = crReporte;
+                    await CargarCRViewerAsync(fechaInicio, fechaFin);
+                    DetenerProgressBar();
+                    HabilitarButtonsYDateTimePicker();
                 }                
             }
 
@@ -85,7 +172,9 @@ namespace CapaPresentacion
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+
+        private async void button2_Click(object sender, EventArgs e)
         {
             try
             {
@@ -99,43 +188,16 @@ namespace CapaPresentacion
                 fechaFin = new DateTime(dateTimePicker3.Value.Year, dateTimePicker3.Value.Month,
                     dateTimePicker3.Value.Day, 23, 59, 58);
 
-
-
                 if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                 {
+                    DeshabilitarButtonsYDateTimePicker();
+                    IniciarProgressBar();
+
                     string nomArchivo = saveFileDialog1.FileName;
-
-                    //http://aspalliance.com/478_Exporting_to_Excel_in_Crystal_Reports_NET__Perfect_Excel_Exports.3
-                    //https://www.c-sharpcorner.com/UploadFile/mahesh/savefiledialog-in-C-Sharp/
-                    CRReporteChequesNoCobradosParaExportar reporte = new CRReporteChequesNoCobradosParaExportar();
-                    reporte.SetDatabaseLogon("sa", "modomixto", "CRUZ2-THINK", "DBCajaCuentas2");
-                    reporte.SetParameterValue("@fechaInicio", fechaInicio);
-                    reporte.SetParameterValue("@fechaFin", fechaFin);
-
-                    //Ponerle las fechas de busqueda al reporte
-                    TextObject periodoDeBusquedaTextObject = reporte.ReportDefinition.ReportObjects["Text19"] as TextObject;
-                    periodoDeBusquedaTextObject.Text = MuestraFechaDeBusquedaSinLaHora(fechaInicio) + " a " + MuestraFechaDeBusquedaSinLaHora(fechaFin);
-
-
-                    // Declare variables and get the export options.
-                    ExportOptions exportOpts = new ExportOptions();
-                    ExcelFormatOptions excelFormatOpts = new ExcelFormatOptions();
-                    DiskFileDestinationOptions diskOpts = new DiskFileDestinationOptions();
-                    exportOpts = reporte.ExportOptions;
-                    // Set the excel format options.
-                    excelFormatOpts.ExcelUseConstantColumnWidth = false;
-                    excelFormatOpts.ShowGridLines = true;
-
-                    //exportOpts.ExportFormatType = ExportFormatType.ExcelRecord;
-                    exportOpts.ExportFormatType = ExportFormatType.Excel;
-                    exportOpts.FormatOptions = excelFormatOpts;
-                    // Set the disk file options and export.
-                    exportOpts.ExportDestinationType = ExportDestinationType.DiskFile;
-                    //diskOpts.DiskFileName = "miotroreporttttte.xls";
-                    diskOpts.DiskFileName = nomArchivo;
-                    exportOpts.DestinationOptions = diskOpts;
-                    reporte.Export();
-
+                    await GenerarReporteExcelAsync(fechaInicio, fechaFin, nomArchivo);
+                    
+                    DetenerProgressBar();
+                    HabilitarButtonsYDateTimePicker();
                     MessageBox.Show("Exportacion lista", "Resultado de operación", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
@@ -144,7 +206,6 @@ namespace CapaPresentacion
             {
                 MessageBox.Show(ex.Message + " " + ex.Source + " " + ex.StackTrace);
             }
-
         }
     }
 }
